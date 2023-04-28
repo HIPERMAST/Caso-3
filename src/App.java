89fbbe62ca92f1d7642f7cdd750f6a3de2dc67f8
Daniel Bernal <da.bernalc1@uniandes.edu.co>
2023-04-27 23:19:48 -0500

    Co-authored-by: lojedaa <lojedaa@users.noreply.github.com> Almost done

--- src/App.java
+++ src/App.java
@@ -2,8 +2,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
+
+
 
 public class App {
 
@@ -16,23 +21,20 @@ public class App {
         String code = parameters.get("Code");
         String sal = parameters.get("SAL");
         int threadNumber = Integer.parseInt(parameters.get("ThreadNumber"));
-        Decypher references = new Decypher(hashAlgorithm, code, sal);
 
-        if (threadNumber == 1) {
-            saveStringMode1(hashAlgorithm, code, sal);
-        } else if (threadNumber == 2) {
-            saveStringMode2(hashAlgorithm, code, sal);
-        }
+        saveString(hashAlgorithm, code, sal, threadNumber);
 
         mode.close();
     }
 
 
-    public static void saveStringMode1(String hashAlgorithm, String code, String sal) {
+    public static void saveString(String hashAlgorithm, String code, String sal, int threadNumber) {
+
+        Decypher decypher = new Decypher(hashAlgorithm, code, sal);
         
-        String originalString = Decypher.findString();
+        String originalString = findString(decypher, threadNumber);
         
-        String fileRoot = "./References/string.txt";
+        String fileRoot = "./strings/"+ threadNumber +".txt";
 
         try {
             File archivo = new File(fileRoot);
@@ -45,22 +47,92 @@ public class App {
         }
     }
 
-    public static void saveStringMode2(String hashAlgorithm, String code, String sal) {
-        
-        String originalString = Decypher.findString();
-        
-        String fileRoot = "./References/string.txt";
 
-        try {
-            File archivo = new File(fileRoot);
-            FileWriter writer = new FileWriter(archivo);
-            writer.write(originalString);
-            writer.close();
-            System.out.println("Cadena guardada exitosamente.");
-        } catch (IOException e) {
-            System.out.println("Ocurrió un error al guardar el archivo: " + e.getMessage());
+    public static String findString( Decypher decypher,int threadNumber) {
+
+        String originalString = "";
+        ArrayList<String> result = new ArrayList<String>();
+
+        result.add(originalString);
+        result.add("0");
+
+        if (threadNumber == 1){
+            int i = 0;
+
+            while (i < 7 && result.get(1) != "1") {
+
+                char[] arr = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+                result = combinationsWithRepetition(arr, i+1, decypher, result);
+
+                i++;
+            }
+            if (result.get(1) == "1") {
+                return originalString = result.get(0);
+            }
+
         }
+        else{
+            // if (Thread.getId() == 1){
+            //     int i = 0;
+
+            //     while (i < 7 && result.get(1) != "1") {
+    
+            //         char[] arr = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+            //         result = combinationsWithRepetition(arr, i+1, decypher, result);
+    
+            //         i+=2;
+            //     }
+            //     if (result.get(1) == "1") {
+            //         return originalString = result.get(0);
+            //     }
+            // }
+            // else {
+            //     int i = 1;
+
+            //     while (i < 7 && result.get(1) != "1") {
+    
+            //         char[] arr = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+            //         result = combinationsWithRepetition(arr, i+1, decypher, result);
+    
+            //         i+=2;
+            //     }
+            //     if (result.get(1) == "1") {
+            //         return originalString = result.get(0);
+            //     }
+            // }
+        }
+        
+
+        return ("No se encontro la cadena.");
     }
+
+    public static ArrayList<String> combinationsWithRepetition(char[] arr, int len, Decypher decypher, ArrayList<String> result) {
+        int[] indices = new int[len];
+        Arrays.fill(indices, 0);
+        while (indices[0] < arr.length && result.get(1) != "1") {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < len; i++) {
+                sb.append(arr[indices[i]]);
+            }
+            result.set(0, sb.toString());
+            result = decypher.getString(result);
+
+            int k = len - 1;
+            while (k >= 0 && indices[k] == arr.length - 1 && result.get(1) != "1") {
+                k--;
+            }
+            if (k < 0) {
+                break;
+            }
+            indices[k]++;
+            for (int i = k + 1; i < len && result.get(1) != "1"; i++) {
+                indices[i] = 0;
+            }
+        }
+
+        return result;
+    }
+
 
     
 
--- src/Decypher.java
+++ src/Decypher.java
@@ -1,9 +1,13 @@
-
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 public class Decypher {
 
-    private String hashAlgorithm;
-    private String code;
-    private String sal;
+    private static String hashAlgorithm;
+    private static String code;
+    private static String sal;
 
     public Decypher(String hashAlgorithm, String code, String sal) {
         this.hashAlgorithm = hashAlgorithm;
@@ -11,10 +15,46 @@ public class Decypher {
         this.sal = sal;
     }
 
-    public static String findString() {
-       
 
 
-        return "";
+    public ArrayList<String> getString(ArrayList<String> result) {
+
+        String originalString = result.get(0);
+
+        String stringSal = originalString + sal;
+
+        String hashedString = "";
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance(hashAlgorithm);
+            byte[] encodedString = stringSal.getBytes(StandardCharsets.UTF_8);
+            byte[] hash = digest.digest(encodedString);
+            hashedString = bytesToHex(hash);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Ocurrió un error al decifrar la cadena: " + e.getMessage());
+        }
+
+        result.set(0, originalString);
+
+        if (hashedString.equals(code)) {
+            System.out.println("La cadena es: " + originalString);
+            result.set(1, "1");
+            return result;
+        }
+
+        return result;
     }
+
+    public String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1)
+                hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+
 }
